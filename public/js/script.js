a2fb9b20e4245d2152eeff71a131e3e544348855
Dinh Xuan <xuanyeumy123@gmail.com>
2025-03-16 11:19:11 +0700

    Thêm tour vào giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,4 @@
 // Slider Tour Detail Page
-console.log("Swiper loaded?", typeof Swiper !== "undefined");
-
 var swiper = new Swiper(".mySwiper", {
   loop: true,
   spaceBetween: 10,
@@ -20,3 +18,50 @@ var swiper2 = new Swiper(".mySwiper2", {
   },
 });
 // End Slider Tour Detail Page
+
+// Carts
+// Kiểm tra giỏ hàng trong localStorage
+let cart = localStorage.getItem("cart");
+
+// Nếu chưa có, tạo mới giỏ hàng rỗng
+if (!cart) {
+  localStorage.setItem("cart", JSON.stringify([]));
+} else {
+  cart = JSON.parse(cart); // Chuyển về mảng để thao tác
+}
+
+// Lấy form giỏ hàng
+const formCart = document.getElementById('cartForm');
+if (formCart) {
+  formCart.addEventListener("submit", (event) => {
+    event.preventDefault(); // Ngăn chặn reload trang
+
+    const quantity = parseInt(event.target.elements.quantity.value);
+    const tourID = parseInt(event.target.elements.tour_id.value);
+
+    if (!tourID || quantity <= 0) {
+      alert("Vui lòng chọn số lượng hợp lệ!");
+      return;
+    }
+
+    // Thêm sản phẩm vào giỏ hàng
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    const indexExistTour = cart.findIndex(item => item.tourID == tourID);
+
+    if(indexExistTour == -1){
+      cart.push({ 
+        tourID: tourID, 
+        quantity: quantity
+      });
+    } else {
+      cart[indexExistTour].quantity = cart[indexExistTour].quantity + quantity;
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    console.log("Thêm vào giỏ hàng:", { tourID, quantity });
+    alert("Đã thêm vào giỏ hàng!");
+
+  });
+}
+// End Carts
